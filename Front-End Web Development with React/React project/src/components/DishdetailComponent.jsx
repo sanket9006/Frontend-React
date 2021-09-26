2a9fbe672a46c40ad837e973821a871ec32294ab
Sanket Patil <sanket.9006@gmail.com>
2021-09-26 11:32:34 +0530

    Update DishdetailComponent.jsx

--- Front-End Web Development with React/React project/src/components/DishdetailComponent.jsx
+++ Front-End Web Development with React/React project/src/components/DishdetailComponent.jsx
@@ -1,58 +1,55 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Card, CardImg, CardTitle, CardBody } from 'reactstrap';
 
-export default class DishDetailComponent extends Component {
-    constructor(props) {
-        super(props)
-        console.log("something is fishy")
-    }
+const DishDetailComponent = (props) => {
 
-    render() {
-        const dish = this.props.dish;
+    const dish = props.dish;
 
-        if (dish != null) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div kay={dish.id} className="col-12 col-md-5  m-1">
-                            <Card >
-                                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                    <CardTitle>
-                                        {dish.name}
-                                    </CardTitle>
-                                    <p>{dish.description}</p>
-                                    {console.log(dish.comments)}
-                                </CardBody>
-                            </Card>
-                        </div >
-                        <div className='col-12 col-md-5 m-2'>
-                            <h4> Comments </h4>
-                            <ul >
-                                {dish.comments.map((comment) => {
-                                    return (
-                                        <li key={comment.id}>
-                                            <p>{comment.comment}</p>
-                                            <p> -- {comment.author},
-                                                &nbsp;
-                                                {new Intl.DateTimeFormat('en-US', {
-                                                    year: 'numeric',
-                                                    month: 'short',
-                                                    day: '2-digit'
-                                                }).format(new Date(comment.date))}
-                                            </p>
-                                        </li>)
-                                })}
-                            </ul>
+    if (dish != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div kay={dish.id} className="col-12 col-md-5  m-1">
+                        <Card >
+                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>
+                                    {dish.name}
+                                </CardTitle>
+                                <p>{dish.description}</p>
+                                {console.log(dish.comments)}
+                            </CardBody>
+                        </Card>
+                    </div >
+                    <div className='col-12 col-md-5 m-2'>
+                        <h4> Comments </h4>
+                        <ul >
+                            {dish.comments.map((comment) => {
+                                return (
+                                    <li key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p> -- {comment.author},
+                                            &nbsp;
+                                            {new Intl.DateTimeFormat('en-US', {
+                                                year: 'numeric',
+                                                month: 'short',
+                                                day: '2-digit'
+                                            }).format(new Date(comment.date))}
+                                        </p>
+                                    </li>)
+                            })}
+                        </ul>
 
-                        </div>
                     </div>
                 </div>
-            )
-        } else {
-            return (
-                <div></div>
-            )
-        }
+            </div>
+        )
+    } else {
+        return (
+            <div></div>
+        )
     }
 }
+
+export default DishDetailComponent;
+
