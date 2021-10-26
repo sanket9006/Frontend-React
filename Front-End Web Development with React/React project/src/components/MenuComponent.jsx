535feb4d4f2b5e2fdf200285311a6265dbfee353
Sanket Patil <sanket.9006@gmail.com>
2021-10-26 11:29:31 +0530

    Update

--- Front-End Web Development with React/React project/src/components/MenuComponent.jsx
+++ Front-End Web Development with React/React project/src/components/MenuComponent.jsx
@@ -1,45 +1,29 @@
 import React, { Component } from 'react'
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-export default class Nav extends Component {
-
-    constructor(props) {
-        super(props)
-
-        console.log(props)
-        console.log('Menu component Constructor is invoked')
-    }
-
-    componentDidMount() {
-        console.log('Menu component componentDidMount is invoked')
-
-    }
-
-    render() {
-
-        console.log('Menu component render is invoked')
-
-        const menu = this.props.dishes.map((dish, id) => {
-            return (
-                <div kay={dish.id} className="col-12 col-md-5 mt-4">
-                    <Card onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>
-                                {dish.name}
-                            </CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div >
-            )
-        });
-
+const Nav = (props) => {
+    const menu = props.dishes.map((dish, id) => {
         return (
-            <div className="container" >
-                <div className="row">
-                    {menu}
-                </div>
-            </div>
+            <div kay={dish.id} className="col-12 col-md-5 mt-4">
+                <Card onClick={() => props.onClick(dish.id)}>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>
+                            {dish.name}
+                        </CardTitle>
+                    </CardImgOverlay>
+                </Card>
+            </div >
         )
-    }
+    });
+
+    return (
+        <div className="container" >
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    )
 }
+
+export default Nav
